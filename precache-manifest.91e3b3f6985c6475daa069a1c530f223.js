0af2b49175f217fd6690fc91d7de36c97718cbc2
rovo486 <rovo486@gmail.com>
2020-05-21 11:13:47 +0900

    Updates

---
+++ precache-manifest.91e3b3f6985c6475daa069a1c530f223.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0da0107fbe0e09651dd4411fcbeab4",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "ed9898f90394b007c4fd",
+    "url": "/movie-app/static/css/main.9044bf26.chunk.css"
+  },
+  {
+    "revision": "2749ee1431ff0e45095c",
+    "url": "/movie-app/static/js/2.40ed9ca4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.40ed9ca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed9898f90394b007c4fd",
+    "url": "/movie-app/static/js/main.e05e7bb5.chunk.js"
+  },
+  {
+    "revision": "255bee9b5e55438d0d88",
+    "url": "/movie-app/static/js/runtime-main.4f650522.js"
+  }
+]);
